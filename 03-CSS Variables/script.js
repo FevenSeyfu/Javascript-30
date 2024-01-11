45e0a941dae97685206d02d69c02c13040771e4f
FevenSeyfu <fevenseyfu00@gmail.com>
2024-01-11 22:17:31 +0300

    change text color

--- 03-CSS Variables/script.js
+++ 03-CSS Variables/script.js
@@ -2,6 +2,7 @@ const spacing = document.querySelector('#spacing');
 const blur = document.querySelector('#blur');
 const color = document.querySelector('#coloring');
 const img =  document.querySelector('.img');
+const text =  document.querySelector('.text')
 
 blur.addEventListener('input',(e)=>{
     img.style.filter = `blur(${e.target.value}px)`
@@ -13,4 +14,5 @@ spacing.addEventListener('input',(e)=>{
 
 color.addEventListener('input',(e)=>{
     img.style.borderColor = e.target.value;
+    text.style.color = e.target.value
 })
