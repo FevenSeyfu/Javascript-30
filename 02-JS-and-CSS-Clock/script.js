24a52a87698013f8cd8a7d923c3fcd28d07c4c9c
FevenSeyfu <fevenseyfu00@gmail.com>
2024-01-15 14:56:41 +0300

    update  clock

--- 02-JS-and-CSS-Clock/script.js
+++ 02-JS-and-CSS-Clock/script.js
@@ -1,23 +1,53 @@
-const hourHandle =document.querySelector('.hour-hand')
-const minuteHandle = document.querySelector('.minute-hand')
-const secondHandle = document.querySelector('.second-hand')
+const body = document.body;
+const toggleBtn = document.querySelector(".theme-toggle");
 
-setInterval(()=>{
-    const date = new Date();
-    // get time
-    let hour  = date.getHours();
-    let minute =  date.getMinutes()
-    let second =  date.getSeconds() 
-    
-    // calculate the clock hand angle
-    let secondAngel =  second*6
-    let minuteAngel  = minute*6;
-    let hourAngel  = (hour * 30)+(minute * 0.5)
+const clock = document.querySelector(".clock-container");
+const hour = document.querySelector(".hour");
+const minute = document.querySelector(".minute");
+const second = document.querySelector(".second");
 
-    //rotate clock hand
-    hourHandle.style.transform = `rotate(${hourAngel}deg)`;
-    minuteHandle.style.transform = `rotate(${minuteAngel}deg)`
-    secondHandle.style.transform = `rotate(${secondAngel}deg)`
-},1000)
+const date = document.querySelector(".date");
+const time = document.querySelector(".time");
+const dateNum = document.createElement("span");
+dateNum.classList.add("dateNum");
 
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  minute.classList.toggle("dark");
+  hour.classList.toggle("dark");
+  clock.classList.toggle("dark");
+  date.classList.toggle("dark");
+  time.classList.toggle("dark");
+  dateNum.classList.toggle("dark");
+  toggleBtn.classList.toggle("theme-toggle--toggled");
+});
 
+setInterval(() => {
+  let d, hr, min, sec;
+  let hr_rotation, min_rotation, sec_rotation;
+
+  d = new Date();
+  hr = d.getHours();
+  min = d.getMinutes();
+  sec = d.getSeconds();
+
+  let day = d.toLocaleDateString("default", { weekday: "long" });
+  let month = d.toLocaleString("default", { month: "long" });
+
+  time.innerHTML = d.toLocaleTimeString("default", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+
+  dateNum.innerHTML = d.getDate();
+  date.innerHTML = day + " , " + month.slice(0, 3);
+  date.appendChild(dateNum);
+  hr_rotation = 30 * hr + min / 2;
+  min_rotation = 6 * min;
+  sec_rotation = 6 * sec;
+
+  hour.style.transform = `rotate(${hr_rotation}deg)`;
+  minute.style.transform = `rotate(${min_rotation}deg)`;
+  second.style.transform = `rotate(${sec_rotation}deg)`;
+}, 1000);
