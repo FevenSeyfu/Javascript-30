8c52970e6e8b6e80b11297b89ae421eea241b779
FevenSeyfu <fevenseyfu00@gmail.com>
2024-01-19 18:51:34 +0300

    complete delete task

--- 10-ToDo-List/script.js
+++ 10-ToDo-List/script.js
@@ -1,5 +1,4 @@
 const taskForm = document.querySelector(".task-form");
-const deleteBtn = document.querySelector(".delete-btn");
 const taskLists = document.querySelector(".tasks-list");
 const Progress = document.querySelector(".progress");
 const addTaskBtn = document.querySelector(".add-task");
@@ -13,28 +12,31 @@ const displayTasks = () => {
   //  fetch and display tasks
   let TaskStr = localStorage.getItem("tasks");
   let Tasks = JSON.parse(TaskStr);
-  Tasks.length>0 ? (Tasks.forEach((task) => {
-    const taskLi = document.createElement("li");
-    taskLi.classList.add("task-item");
-    taskLi.setAttribute("key", task.id);
-    taskLi.innerHTML += `
+  Tasks.length > 0
+    ? Tasks.forEach((task) => {
+        const taskLi = document.createElement("li");
+        taskLi.classList.add("task-item");
+        taskLi.setAttribute("key", task.id);
+        taskLi.innerHTML += `
     <label class="checkbox">
-        <input type="checkbox" class="checkbox-input" ${task.progress === "done" ? "checked" : ""}/>
+        <input type="checkbox" class="checkbox-input" ${
+          task.progress === "done" ? "checked" : ""
+        }/>
         <span class="checkbox-inner"></span>
         <span class="task-name ${task.progress === "done" ? "checked" : ""}">${
-      task.name
-    }</span>
+          task.name
+        }</span>
     </label>
     <button class="delete-btn">
         <i
-        class="fa fa-trash"
+        class="fa fa-trash delete-icon"
         style="font-size: 24px; color: red"
-        class="delete-icon"
         ></i>
     </button>
     `;
-    taskLists.appendChild(taskLi);
-  })):(taskLists.innerHTML+= '<p>Not Task Added!</p>')
+        taskLists.appendChild(taskLi);
+      })
+    : (taskLists.innerHTML += "<p>Not Task Added!</p>");
 };
 //
 const AddTask = (e) => {
@@ -61,13 +63,21 @@ const markCompleted = (e) => {
   let Tasks = JSON.parse(localStorage.getItem("tasks"));
   const taskId = e.target.parentElement.parentElement.getAttribute("key");
   const taskIndex = Tasks.findIndex((task) => task.id === taskId);
-    
-    if (taskIndex !== -1) {
-        Tasks[taskIndex].progress = Tasks[taskIndex].progress === 'todo'? 'done' : 'todo';
-        localStorage.setItem("tasks", JSON.stringify(Tasks));
-        location.reload()
-    }
-   
+
+  if (taskIndex !== -1) {
+    Tasks[taskIndex].progress =
+      Tasks[taskIndex].progress === "todo" ? "done" : "todo";
+    localStorage.setItem("tasks", JSON.stringify(Tasks));
+    location.reload();
+  }
+};
+
+const deleteTask = (e) => {
+    const taskId = e.target.parentElement.parentElement.getAttribute("key");
+    let Tasks = JSON.parse(localStorage.getItem("tasks"));
+    const updatedTasks = Tasks.filter((task) => task.id !== taskId);
+    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+    location.reload();
 };
 
 window.addEventListener("load", displayTasks);
@@ -76,5 +86,8 @@ taskForm.addEventListener("submit", AddTask);
 document.addEventListener("click", function (e) {
   if (e.target.classList.contains("checkbox-input")) {
     markCompleted(e);
+  }
+  if (e.target.classList.contains("delete-icon")) {
+    deleteTask(e);
   }
 });
