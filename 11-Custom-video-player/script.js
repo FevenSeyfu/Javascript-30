548b81a3559a78f2de2b825b07bc6e755b183d04
FevenSeyfu <fevenseyfu00@gmail.com>
2024-01-20 21:51:47 +0300

    add view player controller

--- 11-Custom-video-player/script.js
+++ 11-Custom-video-player/script.js
@@ -1,7 +1,22 @@
 const body = document.body;
 const toggleBtn = document.querySelector(".theme-toggle");
+const viewer = document.querySelector(".viewer");
+const volumeIcon = document.querySelector(".volume-icon");
+const playIcon = document.querySelector(".play-pause-icon");
+const volumeSlider = document.querySelector(".player__slider")
 
-toggleBtn.addEventListener("click", () => {
+const toggleTheme = () => {
   body.classList.toggle("dark");
   toggleBtn.classList.toggle("theme-toggle--toggled");
-});
+  viewer.classList.toggle("dark");
+};
+const playPauseToggle = () => {
+  togglePlayer === "play--v1" ? togglePlayer="pause--v1" : togglePlayer="play--v1";
+  playIcon.src = `${iconBaseUrl}${theme}${togglePlayer}.png`;
+};
+const volumeToggle = () => {
+  volumeSlider.classList.toggle('hidden')
+};
+toggleBtn.addEventListener("click", toggleTheme);
+playIcon.addEventListener("click", playPauseToggle);
+volumeIcon.addEventListener("click", volumeToggle);
