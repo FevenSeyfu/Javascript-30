6d62ffae051d39efc37d8314b3b07cd9b17c8a42
FevenSeyfu <fevenseyfu00@gmail.com>
2024-01-21 20:51:40 +0300

    Add play pause video with space button

--- 11-Custom-video-player/script.js
+++ 11-Custom-video-player/script.js
@@ -1,22 +1,30 @@
 const body = document.body;
 const toggleBtn = document.querySelector(".theme-toggle");
-const viewer = document.querySelector(".viewer");
+const video = document.querySelector(".viewer");
 const volumeIcon = document.querySelector(".volume-icon");
 const playIcon = document.querySelector(".play-pause-icon");
 const volumeSlider = document.querySelector(".player__slider")
+const iconBaseUrl = 'https://img.icons8.com/ios-filled/50/FFFFFF/'
+let togglePlayer;
 
 const toggleTheme = () => {
   body.classList.toggle("dark");
   toggleBtn.classList.toggle("theme-toggle--toggled");
-  viewer.classList.toggle("dark");
+  video.classList.toggle("dark");
 };
 const playPauseToggle = () => {
-  togglePlayer === "play--v1" ? togglePlayer="pause--v1" : togglePlayer="play--v1";
-  playIcon.src = `${iconBaseUrl}${theme}${togglePlayer}.png`;
+  togglePlayer === "pause--v1" ? (togglePlayer="play--v1",video.pause()):( togglePlayer="pause--v1", video.play() );
+  playIcon.src = `${iconBaseUrl}${togglePlayer}.png`;
 };
 const volumeToggle = () => {
   volumeSlider.classList.toggle('hidden')
 };
 toggleBtn.addEventListener("click", toggleTheme);
 playIcon.addEventListener("click", playPauseToggle);
+window.addEventListener("keydown", (e)=>{
+  if (e.key === " ") {
+    playPauseToggle()
+  }
+});
 volumeIcon.addEventListener("click", volumeToggle);
+// volumeSlider.addEventListener('change',changeVolume);
